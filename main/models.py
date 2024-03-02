c50db1f294b346e3937acc511bfe37cd56bbfb7e
Дмитрий Макаров <dim370as@gmail.com>
2024-03-02 13:47:59 +0300

    Добавлены базы данных для товара и особого товара.

--- main/models.py
+++ main/models.py
@@ -7,3 +7,14 @@ class Profile(models.Model):
 
 class Vote(models.Model):
     pass
+
+
+class Product(models.Model):
+    type = models.CharField(max_length=200)
+    price = models.IntegerField()
+    incart = models.BooleanField()
+    infavorites = models.BooleanField()
+
+
+class SpecialProduct(models.Model):
+    features = models.ForeignKey(to=Product, on_delete=models.CASCADE)
