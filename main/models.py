24e095dc6c97e6660b99e01fffc797705af5b891
Дмитрий Макаров <dim370as@gmail.com>
2024-03-03 12:43:19 +0300

    Добавлены модели для типа товара, корзины, избранных товаров, статуса товаров

--- main/models.py
+++ main/models.py
@@ -9,11 +9,29 @@ class Vote(models.Model):
     pass
 
 
+class Type(models.Model):
+    name = models.CharField(max_length=200)
+    combining_types = models.JSONField()  # сочетающиеся товары
+    not_combining_types = models.JSONField()  # несочетающиеся товары
+
+
+class Cart(models.Model):
+    products = models.ManyToManyField(to=Type)
+
+
+class Favorites(models.Model):
+    products = models.ManyToManyField(to=Type)
+
+
+class ProductsStatus(models.Model):  # Статус товаров
+    profile = models.OneToOneField(to=Profile, on_delete=models.CASCADE)  # пользователь
+    cart = models.OneToOneField(to=Cart, on_delete=models.CASCADE)  # корзина
+    favorites = models.OneToOneField(to=Favorites, on_delete=models.CASCADE)  # избранное
+
+
 class Product(models.Model):
-    type = models.CharField(max_length=200)
+    type = models.ForeignKey(to=Type, on_delete=models.CASCADE)
     price = models.IntegerField()
-    incart = models.BooleanField()
-    infavorites = models.BooleanField()
 
 
 class SpecialProduct(models.Model):
