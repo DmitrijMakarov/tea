4d125c00ff9ae0c3e7bab0d553bc3436d095f6e2
Egor Kosyakov <bebrik450.g@gmail.com>
2024-03-16 18:29:05 +0000

    Перенесен urls.py из simple votings

--- simple_votings/urls.py
+++ simple_votings/urls.py
@@ -6,8 +6,15 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create_survey_script/', views.create_survey, name='create_survey_script'),
+    path('delete_surveys_script/', views.delete_surveys, name='delete_surveys_script'),
     path('', views.index_page, name='index'),
-    path('time/', views.time_page, name='time'),
+    path('vote/create', views.create_vote_page, name='create_vote'),
+    path('vote/answer/<id>', views.vote_answer, name='answer'),
+    path('vote/<id>', views.vote_view, name='vote'),
     path('login/', views.login_page, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('signup/', views.reg_page, name='registration'),
+    path('profile/', views.profile_page, name='profile'),
+    path('logout/', views.log_out, name='logout'),
+    path('error-404', views.page_404, name='error-404')
 ]
