b399e4f39f959677e97f6a2c4b1b1e03ba1dd4b4
Анна Цой <Anna.Tsoy.j21@gmail.com>
2024-04-05 22:50:04 +0300

    немного почистила views.py

--- main/views.py
+++ main/views.py
@@ -4,13 +4,6 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 
 
-def get_menu_context():
-    return [
-        {'url_name': 'index', 'name': 'Главная'},
-        {'url_name': 'time', 'name': 'Текущее время'},
-    ]
-
-
 def index_page(request: WSGIRequest):
     context = {}
     return render(request, 'pages/index.html', context)
