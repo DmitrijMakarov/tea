1a01f025ef5283ddb7bba1be51e3f7161c6e7db9
Анна Цой <Anna.Tsoy.j21@gmail.com>
2024-04-06 17:27:59 +0300

    новая версия views.py и urls.py

--- simple_votings/urls.py
+++ simple_votings/urls.py
@@ -5,8 +5,12 @@ from main import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', views.index_page, name='index'),
-    path('blogs/', views.blogs_page, name='blogs'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+   path('admin/', admin.site.urls),
+   path('', views.index_page, name='index'),
+   path('blogs/', views.blogs_page, name='blogs'),
+   path('login/', views.login_page, name='login'),
+   path('delivery/', views.delivery_page, name='delivery'),
+   path('card_product/', views.card_product, name='card_product'),
+   path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
+
