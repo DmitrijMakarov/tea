e0fdfe3eb2bffb73bcaa03c4667d73457c8aace1
Анна Цой <Anna.Tsoy.j21@gmail.com>
2024-04-06 17:05:53 +0300

    новая версия views.py и urls.py

--- main/views.py
+++ main/views.py
@@ -3,13 +3,21 @@ from django.shortcuts import render
 
 
 def index_page(request: WSGIRequest):
-    context = {}
-    return render(request, 'pages/index.html', context)
+   context = {}
+   return render(request, 'pages/index.html', context)
 
 def blogs_page(request: WSGIRequest):
-    context = {}
-    return render(request, 'pages/blogs.html', context)
+   context = {}
+   return render(request, 'pages/blogs.html', context)
 
 def login_page(request: WSGIRequest):
-    context = {}
-    return render(request, 'registration/login.html', context)
+   context = {}
+   return render(request, 'registration/login.html', context)
+
+def delivery_page(request: WSGIRequest):
+   context = {}
+   return render(request, 'pages/delivery.html', context)
+
+def card_product(request: WSGIRequest):
+   context = {}
+   return render(request, 'pages/card_product.html', context)
