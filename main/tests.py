a4bdc40556787f715b247d702fba7b2eb7057a3a
slawsva <svyatoslavsakulin@mail.ru>
2024-04-07 10:57:16 +0300

    Новое тестирование django

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        response_admin = self.client.get('/admin/')
+        response_blogs = self.client.get('/blogs/')
+        response_delivery = self.client.get('/delivery/')
+        response_login = self.client.get('/login/')
+        response_card_product = self.client.get('/card_product/')
+        response_logout = self.client.get('/logout/')
+        response_index = self.client.get('')
+
+        self.assertEqual(response_admin.status_code, 200)
+        self.assertEqual(response_blogs.status_code, 200)
+        self.assertEqual(response_delivery.status_code, 200)
+        self.assertEqual(response_login.status_code, 200)
+        self.assertEqual(response_card_product.status_code, 200)
+        self.assertEqual(response_logout.status_code, 200)
+        self.assertEqual(response_index.status_code, 200)
