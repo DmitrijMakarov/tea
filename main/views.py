f0ea1a82ead08abcb1c93575e6f4162c932c10fa
Кучук Егор <kuchuk@informatics.ru>
2024-04-27 18:35:40 +0300

    Авторизация пользователя

--- main/views.py
+++ main/views.py
@@ -1,27 +1,51 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+
+from main.forms import LoginUserForm
 
 
 def index_page(request: WSGIRequest):
-   context = {}
-   return render(request, 'pages/index.html', context)
+    context = {}
+    return render(request, 'pages/index.html', context)
+
 
 def blog_card_page(request: WSGIRequest):
-   context = {}
-   return render(request, 'pages/blog_card.html', context)
+    context = {}
+    return render(request, 'pages/blog_card.html', context)
+
 
 def blogs_page(request: WSGIRequest):
-   context = {}
-   return render(request, 'pages/blogs.html', context)
+    context = {}
+    return render(request, 'pages/blogs.html', context)
+
 
 def login_page(request: WSGIRequest):
-   context = {}
-   return render(request, 'registration/login.html', context)
+    context = {
+        "form": LoginUserForm(),
+    }
+    if request.method == "POST":
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            logging_user = authenticate(username=form.username, password=form.password)
+            if logging_user is not None:
+                login(request, logging_user)
+                return redirect('')
+
+            # TODO: Вывод о несуществовании такого пользователя
+            return render(request, 'registration/login.html', context)
+        else:
+            context["form"] = form  # Внутри формы содержатся ошибки
+        return render(request, 'registration/login.html', context)
+
+    return render(request, 'registration/login.html', context)
+
 
 def delivery_page(request: WSGIRequest):
-   context = {}
-   return render(request, 'pages/delivery.html', context)
+    context = {}
+    return render(request, 'pages/delivery.html', context)
+
 
 def card_product(request: WSGIRequest):
-   context = {}
-   return render(request, 'pages/card_product.html', context)
+    context = {}
+    return render(request, 'pages/card_product.html', context)
