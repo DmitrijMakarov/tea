251cd143fc0d62a22a9ee603381f1773396befd4
Анна Цой <Anna.Tsoy.j21@gmail.com>
2024-04-28 05:10:02 +0300

    новая бд, не факт, что пойдёт в сайт, я просто пытаюсь настроить отображение инфы по чаю через бд

--- main/models.py
+++ main/models.py
@@ -1,37 +1,12 @@
 from django.db import models
 
 
-class Profile(models.Model):
-    pass
-
-
-class Vote(models.Model):
-    pass
-
-
-class Type(models.Model):
-    name = models.CharField(max_length=200)
-    products = models.JSONField()  # список сочетающихся и несочетающихся товаров
-
-
-class Cart(models.Model):
-    products = models.ManyToManyField(to=Type)
-
-
-class Favorites(models.Model):
-    products = models.ManyToManyField(to=Type)
-
-
-class ProductsStatus(models.Model):  # Статус товаров
-    profile = models.OneToOneField(to=Profile, on_delete=models.CASCADE)  # пользователь
-    cart = models.OneToOneField(to=Cart, on_delete=models.CASCADE)  # корзина
-    favorites = models.OneToOneField(to=Favorites, on_delete=models.CASCADE)  # избранное
-
-
 class Product(models.Model):
-    type = models.ForeignKey(to=Type, on_delete=models.CASCADE)
-    price = models.IntegerField()
+    name = models.CharField("Название", max_length=50)
+    type = models.CharField("Вид", max_length=20)
+    price = models.IntegerField("Цена")
+    description = models.TextField("Описание", max_length=100)
 
-
-class SpecialProduct(models.Model):
-    features = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
